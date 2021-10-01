63c3a4dda4b728dbe2349ad872c353b24679261c
Joao Porfirio <joaoprf54@gmail.com>
2021-09-30 22:23:33 -0300

    Estrutura componente pronta

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import banner from '../../assets/image/ilustracao.png';
+import playIcon from '../../assets/image/i-play.png';
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 const Main = () => {
   return (
     <main className="main">
       <section className="main__container">
         <article className="main__conteudo">
-          <h5>HAYAO MIYAZAKI</h5>
-          <h1>A VIAGEM <br/>DE CHIHIRO</h1>
-          <p>
+          <h5 className="main__pre-title">HAYAO MIYAZAKI</h5>
+          <h1 className="main__title">A VIAGEM <br/>DE CHIHIRO</h1>
+          <p className="main__info">
             Chihiro chega a um mundo mágico dominado por <br/>uma bruxa. Aqueles que a desobedecem são<br/> transformados em animais.
           </p>
-
-          <button>Assistir agora</button>
-          <button>assista o trailer</button>
+          <div className="main__buttons">
+            <button className="main__button-now">
+            <i className="fa fa-home"></i>assistir agora</button>
+            <button className="main__button-trailer">assista o trailer</button>
+          </div>
         </article>
         <article className="main__banner">
-          <img src={banner} alt="" />
+          <img className="main__banner-img" src={banner} alt="" />
         </article>
       </section>
     </main>
