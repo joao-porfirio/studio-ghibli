7d67f9b893152ec04d92d674a75737a44de25a4e
Joao Porfirio <joaoprf54@gmail.com>
2021-10-05 10:43:09 -0300

    Estrutura final componente Main

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import banner from '../../assets/image/ilustracao.png';
 import playIcon from '../../assets/image/i-play.png';
+import Button from '../Button/Button';
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 const Main = () => {
   return (
@@ -10,11 +11,12 @@ const Main = () => {
           <h5 className="main__pre-title">HAYAO MIYAZAKI</h5>
           <h1 className="main__title">A VIAGEM <br/>DE CHIHIRO</h1>
           <p className="main__info">
-            Chihiro chega a um mundo mágico dominado por <br/>uma bruxa. Aqueles que a desobedecem são<br/> transformados em animais.
+            Chihiro chega a um mundo mágico dominado por uma bruxa. Aqueles que a desobedecem são transformados em animais.
           </p>
           <div className="main__buttons">
-            <button className="main__button-now">
-            <i className="fa fa-home"></i>assistir agora</button>
+            {/* <button className="main__button-now">
+            <i className="fa fa-home"></i>assistir agora</button> */}
+            <Button icon={playIcon}/>
             <button className="main__button-trailer">assista o trailer</button>
           </div>
         </article>
