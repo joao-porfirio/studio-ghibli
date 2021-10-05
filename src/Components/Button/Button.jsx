cde6c6b244171c604a4c25634497349c602aed35
Joao Porfirio <joaoprf54@gmail.com>
2021-10-05 10:42:30 -0300

    Componente Button para inserção de icon na tela principal

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = (props) => {
+  return (
+  <button className="main__button-now">
+    <img className="icon-button" alt="" src={props.icon} />assistir agora
+  </button>
+  )
+}
+
+export default Button
